4e4dae0804687fb5a31f59cf5dee54e9524b40ad
Andy Cochran <acochran@openplans.org>
2014-01-14 17:32:54 -0500

    add views for product page, signup, signin, and password reset

--- src/planbox_ui/urls.py
+++ src/planbox_ui/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
-from .views import index_view, project_view
+from .views import index_view, project_view, signup_view, signin_view, password_reset_view
 
 urlpatterns = patterns('',
 
     url(r'^project/', project_view, name='app-project'),
+    url(r'^signup/$', signup_view, name='app-signup'),
+    url(r'^signin/$', signin_view, name='app-signin'),
+    url(r'^password-reset/$', password_reset_view, name='app-password-reset'),
     url(r'^$', index_view, name='app-index'),
 )
--- src/planbox_ui/views.py
+++ src/planbox_ui/views.py
@@ -6,9 +6,24 @@ class IndexView (TemplateView):
     template_name = 'index.html'
 
 
+class SignupView (TemplateView):
+    template_name = 'signup.html'
+
+
+class PasswordResetView (TemplateView):
+    template_name = 'password-reset.html'
+
+
+class SigninView (TemplateView):
+    template_name = 'signin.html'
+
+
 class ProjectView (TemplateView):
     template_name = 'project.html'
 
 # App views
 index_view = IndexView.as_view()
 project_view = ProjectView.as_view()
+signup_view = SignupView.as_view()
+signin_view = SigninView.as_view()
+password_reset_view = PasswordResetView.as_view()
