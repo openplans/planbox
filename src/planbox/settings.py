1998ccd21363abea607ab2074c1454e2819778ca
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-01-22 15:25:06 -0500

    More Heroku setup - Add a heroku_wsgi module for running static.cling - Add GZipping - Add helpful Heroku-related modules to the requirements

--- src/planbox/settings.py
+++ src/planbox/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = (
 LOGIN_URL = 'app-signin'
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
