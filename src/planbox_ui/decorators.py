ec80290ab26adb37f2f2aaa60a29da22f66243a4
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-01-23 11:53:30 -0500

    Add ssl_required decorator

---
+++ src/planbox_ui/decorators.py
@@ -0,0 +1,28 @@
+"""
+ssl_required decorator
+----------------------
+Redirect requests to the given to to HTTPS if they are not secure already.
+Source: https://djangosnippets.org/snippets/1351/
+Author: pjs (https://djangosnippets.org/users/pjs/)
+"""
+
+import urlparse
+from django.conf import settings
+from django.http import HttpResponseRedirect
+
+
+def ssl_required(view_func):
+    def _checkssl(request, *args, **kwargs):
+        if not settings.DEBUG and not request.is_secure():
+            if hasattr(settings, 'SSL_DOMAIN'):
+                url_str = urlparse.urljoin(
+                    settings.SSL_DOMAIN,
+                    request.get_full_path()
+                )
+            else:
+                url_str = request.build_absolute_uri()
+            url_str = url_str.replace('http://', 'https://')
+            return HttpResponseRedirect(url_str)
+
+        return view_func(request, *args, **kwargs)
+    return _checkssl
