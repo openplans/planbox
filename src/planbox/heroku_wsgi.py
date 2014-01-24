dd5899dbd3bf52abb61c144b0c8fd172b0cf80d2
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-01-23 22:53:27 -0500

    Add WSGI GZip middleware for static files

--- src/planbox/heroku_wsgi.py
+++ src/planbox/heroku_wsgi.py
@@ -11,5 +11,10 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "planbox.settings")
 
 from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
 from dj_static import Cling
-application = Cling(get_wsgi_application())
+application = Cling(application)
+
+from .gzip_middleware import GzipMiddleware
+application = GzipMiddleware(application)
