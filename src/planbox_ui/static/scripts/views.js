0b1f8057497c9ce5f2e8053f6bd05e0dd6373874
Aaron Ogle <atogle@gmail.com>
2014-02-21 11:41:03 -0500

    Make pasteHtmlAtCaret a common utility - Add utils.js and import it in the template - Update Gruntfile.js - Update references in the view

--- src/planbox_ui/static/scripts/views.js
+++ src/planbox_ui/static/scripts/views.js
@@ -206,60 +206,11 @@ var Planbox = Planbox || {};
       'reorder': 'dataChanged'
     },
     initialize: function() {
-      // http://stackoverflow.com/questions/15125217/convert-html-to-plain-text-in-contenteditable
-      // TODO: Make a common utility
-      function pasteHtmlAtCaret(html, selectPastedContent) {
-        var sel, range;
-        if (window.getSelection) {
-          // IE9 and non-IE
-          sel = window.getSelection();
-          if (sel.getRangeAt && sel.rangeCount) {
-            range = sel.getRangeAt(0);
-            range.deleteContents();
-
-            // Range.createContextualFragment() would be useful here but is
-            // only relatively recently standardized and is not supported in
-            // some browsers (IE9, for one)
-            var el = document.createElement("div");
-            el.innerHTML = html;
-            var frag = document.createDocumentFragment(), node, lastNode;
-            while ( (node = el.firstChild) ) {
-              lastNode = frag.appendChild(node);
-            }
-            var firstNode = frag.firstChild;
-            range.insertNode(frag);
-
-            // Preserve the selection
-            if (lastNode) {
-              range = range.cloneRange();
-              range.setStartAfter(lastNode);
-              if (selectPastedContent) {
-                range.setStartBefore(firstNode);
-              } else {
-                range.collapse(true);
-              }
-              sel.removeAllRanges();
-              sel.addRange(range);
-            }
-          }
-        } else if ( (sel = document.selection) && sel.type !== "Control") {
-          // IE < 9
-          var originalRange = sel.createRange();
-          originalRange.collapse(true);
-          sel.createRange().pasteHTML(html);
-          if (selectPastedContent) {
-            range = sel.createRange();
-            range.setEndPoint("StartToStart", originalRange);
-            range.select();
-          }
-        }
-      }
-
       // Hijack the enter key - consistently use <br> across browsers
       this.$el.on('keydown', '[contenteditable]', function(evt) {
         if( evt.which === 13 ){
           evt.preventDefault();
-          pasteHtmlAtCaret('<br>');
+          NS.Utils.pasteHtmlAtCaret('<br>');
         }
       });
 
@@ -279,7 +230,7 @@ var Planbox = Planbox || {};
         }
 
         // Convert line breaks into <br> and paste
-        pasteHtmlAtCaret(pasted.replace(/\n/g, '<br>'));
+        NS.Utils.pasteHtmlAtCaret(pasted.replace(/\n/g, '<br>'));
       });
     },
     onRender: function() {
