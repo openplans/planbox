3a6da6b307a84e130f0081b68caf5da44604f16a
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-03-23 17:44:33 -0400

    Actually include the timeline section migration

--- src/planbox_data/migrations/0020_add_timeline_sections.py
+++ src/planbox_data/migrations/0020_add_timeline_sections.py
@@ -8,9 +8,16 @@ class Migration(DataMigration):
 
     def forwards(self, orm):
         "Write your forwards methods here."
-        # Note: Don't use "from appname.models import ModelName". 
-        # Use orm.ModelName to refer to models in this application,
-        # and orm['appname.ModelName'] for models in other applications.
+        for project in orm.Project.objects.all():
+            if project.sections.count() == 0:
+                orm.Section.objects.create(
+                    project=project,
+                    type='timeline',
+                    slug='timeline',
+                    index=0,
+                    label='Project Timeline',
+                    menu_label='Timeline',
+                    details={})
 
     def backwards(self, orm):
         "Write your backwards methods here."
