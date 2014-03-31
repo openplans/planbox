c0c8ddcf65d9725cd27d0dd3510724daa7524fe7
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-03-31 19:18:22 -0400

    Bugfix: as_json template filter neutralizes </script> tags

--- src/planbox_ui/templatetags/planbox_utils.py
+++ src/planbox_ui/templatetags/planbox_utils.py
@@ -7,4 +7,8 @@ register = Library()
 
 @register.filter
 def as_json(data):
-    return mark_safe(json.dumps(data, cls=DjangoJSONEncoder))
+    json_data = json.dumps(data, cls=DjangoJSONEncoder)
+    # If we come across </script>, we must assume that it's a part of a string
+    # and neutralize it.
+    json_data = json_data.replace('</script>', '<" + "/script>')
+    return mark_safe(json_data)
