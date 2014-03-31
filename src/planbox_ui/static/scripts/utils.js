4e8d872ea1924f7f53cf430cdb511521d9589856
Aaron Ogle <atogle@gmail.com>
2014-03-31 13:50:08 -0400

    Hotfix: Move admin-only components to their own min file - This lets us use fancier libs for admin without breaking the read-only display - Fix jslint error in utils.js

--- src/planbox_ui/static/scripts/utils.js
+++ src/planbox_ui/static/scripts/utils.js
@@ -41,14 +41,14 @@ var Planbox = Planbox || {};
             sel.addRange(range);
           }
         }
-      } else if ( (sel = document.selection) && sel.type !== "Control") {
+      } else if ( (sel = document.selection) && sel.type !== 'Control') {
         // IE < 9
         var originalRange = sel.createRange();
         originalRange.collapse(true);
         sel.createRange().pasteHTML(html);
         if (selectPastedContent) {
           range = sel.createRange();
-          range.setEndPoint("StartToStart", originalRange);
+          range.setEndPoint('StartToStart', originalRange);
           range.select();
         }
       }
