cd9c5ea987484ebaf5fe464c0ca0b431aa030631
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-04-11 13:33:09 -0400

    Hotfix: add a change password link to the user menu

--- src/password_reset/urls.py
+++ src/password_reset/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from password_reset.views import password_reset_view, password_reset_request_view, password_reset_instructions_view
+from password_reset.views import password_change_view, password_reset_view, password_reset_request_view, password_reset_instructions_view
 
 urlpatterns = patterns('',
+    url(r'^change-password/$', password_change_view, name='password-change'),
     url(r'^reset-password/$', password_reset_request_view, name='password-reset-request'),
     url(r'^reset-password/instructions$', password_reset_instructions_view, name='password-reset-instructions'),
     url(r'^reset-password/(?P<token>[^/]*)$', password_reset_view, name='password-reset'),
