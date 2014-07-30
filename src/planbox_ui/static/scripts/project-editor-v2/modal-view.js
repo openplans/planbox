2c72d0772c0d42833102312985b6d1c9f59c5bce
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-07-30 13:18:22 -0400

    Add a modal view for errors

---
+++ src/planbox_ui/static/scripts/project-editor-v2/modal-view.js
@@ -0,0 +1,34 @@
+/*globals Backbone jQuery Handlebars Modernizr _ Pen FileAPI chrono */
+
+var Planbox = Planbox || {};
+
+(function(NS, $) {
+  'use strict';
+
+  NS.ModalView = Backbone.Marionette.ItemView.extend({
+    template: '#modal-tpl',
+    className: 'reveal-modal medium',
+    attributes: {
+      'data-reveal': ''
+    },
+    onShow: function() {
+      // This is gross. We should encourage Foundation to fix this.
+      this.$el.foundation().foundation('reveal', 'open');
+    },
+    onClose: function() {
+      // This is gross. We should encourage Foundation to fix this.
+      this.$el.foundation().foundation('reveal', 'close');
+    }
+  });
+
+  NS.showErrorModal = function(title, subtitle, description) {
+    NS.app.modalRegion.show(new NS.ModalView({
+      model: new Backbone.Model({
+        title: title,
+        subtitle: subtitle,
+        description: description
+      })
+    }));
+  };
+
+}(Planbox, jQuery));
