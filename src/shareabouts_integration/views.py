68052b201fd6e14e0113855f9cd0916878983765
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-08-05 17:04:44 -0400

    Simplify the shareabouts dataset creation by not worrying about creating cors permissions

--- src/shareabouts_integration/views.py
+++ src/shareabouts_integration/views.py
@@ -21,15 +21,12 @@ def create_dataset(request):
     if len(slug) == 0:
         return HttpResponse(
             json.dumps({'errors': [{'dataset_slug': 'This field is required.'}]}),
-            content_type='application/json',
-            status=400)
+            content_type='application/json', status=400)
 
-    current_origin = request.META.get('HTTP_ORIGIN', '')
     datasets_url = 'https://%s/api/v2/%s/datasets' % (
         settings.SHAREABOUTS_HOST,
         settings.SHAREABOUTS_USERNAME)
     dataset_url = '/'.join([datasets_url, slug])
-    origins_url = '/'.join([dataset_url, 'origins'])
     planbox_auth = HTTPBasicAuth(
         settings.SHAREABOUTS_USERNAME,
         settings.SHAREABOUTS_PASSWORD)
@@ -37,24 +34,15 @@ def create_dataset(request):
     # Try to retrieve the dataset.
     ds_response = requests.get(dataset_url, auth=planbox_auth)
 
+    # If the dataset exists already; nothing to do, since we assume that a
+    # CORS permission profile is already created.
     if ds_response.status_code == 200:
-        if current_origin:
-            # Check whether the dataset  can write to the dataset from this origin.
-            # TODO: Retry request if not 200
-            origins_response = requests.get(origins_url, auth=planbox_auth)
-            patterns = [origin['pattern'] for origin in origins_response.json()['results']]
-
-            # If not, then register this
-            if current_origin not in patterns:
-                requests.post(origins_url,
-                    data=json.dumps({'pattern': current_origin}),
-                    headers={'Content-type': 'application/json'},
-                    auth=planbox_auth)
-
         return HttpResponse(
             json.dumps({'url': ds_response.json()['url']}),
             content_type='application/json')
 
+    # If the dataset was not reported as existing but we didn't get a 404 back
+    # then we have some error response and should send a 502 down.
     elif ds_response.status_code != 404:
         return HttpResponse(
             json.dumps({'errors': 'Unknown upstream problem.'}),
@@ -67,13 +55,7 @@ def create_dataset(request):
         headers={'Content-type': 'application/json'},
         auth=planbox_auth)
 
-    # Check whether we can write to the dataset from this origin.
-    if current_origin:
-        requests.post(origins_url,
-            data=json.dumps({'pattern': current_origin}),
-            headers={'Content-type': 'application/json'},
-            auth=planbox_auth)
-
+    # Check that we were successful in creating the dataset.
     if ds_response.status_code == 201:
         return HttpResponse(
             json.dumps({'url': ds_response.json()['url']}),
@@ -85,6 +67,6 @@ def create_dataset(request):
             content_type='application/json')
 
     else:
-        return HttpResponse(json.dumps({'errors': 'Upstream problem.'}),
-            status=503,
+        return HttpResponse(json.dumps({'errors': 'Unknown upstream problem.'}),
+            status=502,
             content_type='application/json')
