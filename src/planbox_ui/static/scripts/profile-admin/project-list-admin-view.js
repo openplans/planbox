7be5bcb8f90079905312b4b9d572dd069fe5680f
Andy Cochran <acochran@openplans.org>
2014-08-19 16:58:41 -0400

    stub in project slug change form in project list

--- src/planbox_ui/static/scripts/profile-admin/project-list-admin-view.js
+++ src/planbox_ui/static/scripts/profile-admin/project-list-admin-view.js
@@ -7,7 +7,28 @@ var Planbox = Planbox || {};
 
   NS.ProjectListItemAdminView = Backbone.Marionette.ItemView.extend({
     template: '#project-list-item-admin-tpl',
-    tagName: 'li'
+    tagName: 'li',
+    className: 'project',
+    ui: {
+      changeSlugBtn: '.change-slug',
+      cancelSlugBtn: '.cancel-slug-change'
+    },
+    events: {
+      'click @ui.changeSlugBtn': 'handleOpenSlugForm',
+      'click @ui.cancelSlugBtn': 'handleCloseSlugForm'
+    },
+    handleOpenSlugForm: function(evt) {
+      evt.preventDefault();
+      var context = $(evt.currentTarget).parents('li.project');
+      context.find('.slug').addClass('hide');
+      context.find('form').removeClass('hide');
+    },
+    handleCloseSlugForm: function(evt) {
+      evt.preventDefault();
+      var context = $(evt.currentTarget).parents('li.project');
+      context.find('.slug').removeClass('hide');
+      context.find('form').addClass('hide');
+    }
   });
 
   NS.ProjectListAdminView = Backbone.Marionette.CompositeView.extend({
