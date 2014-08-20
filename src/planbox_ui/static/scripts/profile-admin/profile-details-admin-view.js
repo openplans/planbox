a1796c4e77ebd39feeddcf5f53338c374677e5db
Andy Cochran <acochran@openplans.org>
2014-08-20 14:29:50 -0400

    hook up pen and drop area js

--- src/planbox_ui/static/scripts/profile-admin/profile-details-admin-view.js
+++ src/planbox_ui/static/scripts/profile-admin/profile-details-admin-view.js
@@ -5,26 +5,37 @@ var Planbox = Planbox || {};
 (function(NS, $) {
   'use strict';
 
-  NS.ProfileDetailsAdminView = Backbone.Marionette.ItemView.extend({
-    template: '#profile-details-admin-tpl',
-    ui: {
-      changeProfileBtn: '.profile-edit-button',
-      cancelProfileBtn: '.cancel-profile-edit-button'
-    },
-    events: {
-      'click @ui.changeProfileBtn': 'handleOpenProfileForm',
-      'click @ui.cancelProfileBtn': 'handleCloseProfileForm'
-    },
-    handleOpenProfileForm: function(evt) {
-      evt.preventDefault();
-      $('.profile-details').addClass('hide');
-      $('.profile-form').removeClass('hide');
-    },
-    handleCloseProfileForm: function(evt) {
-      evt.preventDefault();
-      $('.profile-details').removeClass('hide');
-      $('.profile-form').addClass('hide');
-    }
-  });
+  NS.ProfileDetailsAdminView = Backbone.Marionette.ItemView.extend(
+    _.extend({}, NS.ImageDropZonesMixin, NS.ContentEditableMixin, {
+      template: '#profile-details-admin-tpl',
+      ui: {
+        imageDropZones: '.image-dnd',
+        editables: '[data-attr]',
+        richEditables: '.rich-editable',
+        changeProfileBtn: '.profile-edit-button',
+        cancelProfileBtn: '.cancel-profile-edit-button'
+      },
+      events: {
+        'blur @ui.editables': 'handleEditableBlur',
+        'input @ui.editables': 'handleEditableBlur',
+        'click @ui.changeProfileBtn': 'handleOpenProfileForm',
+        'click @ui.cancelProfileBtn': 'handleCloseProfileForm'
+      },
+      handleOpenProfileForm: function(evt) {
+        evt.preventDefault();
+        $('.profile-details').addClass('hide');
+        $('.profile-form').removeClass('hide');
+      },
+      handleCloseProfileForm: function(evt) {
+        evt.preventDefault();
+        $('.profile-details').removeClass('hide');
+        $('.profile-form').addClass('hide');
+      },
+      onRender: function() {
+        this.initRichEditables();
+        this.initDropZones();
+      }
+    })
+  );
 
 }(Planbox, jQuery));
